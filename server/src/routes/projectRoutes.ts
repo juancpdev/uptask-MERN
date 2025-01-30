1c7e48cee59a7e3f812734bb2edf5af65d9b6af5
Juan Cruz <juan_pineda_12@hotmail.es>
2025-01-30 16:45:06 -0300

    endpoint UpdateProject

--- server/src/routes/projectRoutes.ts
+++ server/src/routes/projectRoutes.ts
@@ -24,4 +24,16 @@ router.get('/:id',
     ProjectController.getProjectById
 )
 
+router.put('/:id',
+    param('id').isMongoId().withMessage('ID no valido'),
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripcion es obligatoria'),
+    handleInputErrors,
+    ProjectController.updateProject
+)
+
 export default router
