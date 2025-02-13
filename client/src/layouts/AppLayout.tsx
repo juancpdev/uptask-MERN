48f48b4ea6b961fcd941c1d89b703b2a46acde47
Juan Cruz <juan_pineda_12@hotmail.es>
2025-02-13 13:45:18 -0300

    navMenu

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Logo from "@/components/Logo";
+import NavMenu from "@/components/NavMenu";
 
 export default function AppLayout() {
   return (
     <div className="grid min-h-screen grid-rows-[auto_1fr_auto]">
-      <header className="bg-gray-800 py-5 ">
-        <div className=" max-w-screen-2xl mx-auto">     
+      <header className="bg-gray-800 p-5">
+        <div className=" max-w-screen-2xl mx-auto flex flex-col md:flex-row justify-between items-center">     
           <div className="w-64">
             <Logo/>
           </div>
+          <NavMenu/>
         </div>    
       </header>
       
