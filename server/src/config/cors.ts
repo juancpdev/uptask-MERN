ee48d415870823066d89a9f8c7e4a8816b3a82bb
Juan Cruz <juan_pineda_12@hotmail.es>
2025-04-14 19:22:43 -0300

    creacion de model usuario

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -3,12 +3,8 @@ import { CorsOptions } from "cors";
 export const corsConfig : CorsOptions = {
     origin : function(origin, callback) {
         // Lista de orígenes permitidos
-        const whiteList = [process.env.FRONTEND_URL, process.env.BACKEND_URL];
+        const whiteList = [process.env.FRONTEND_URL];
 
-        // Permitir solicitudes sin origen (como las del mismo servidor o herramientas como Postman)
-        if (!origin) {
-            return callback(null, true);
-        }
 
         if(whiteList.includes(origin)) {
             callback(null, true)
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import morgan from 'morgan'
 import { corsConfig } from './config/cors'
 import { connectDB } from './config/db'
+import authRoutes from './routes/authRoutes'
 import projectRoutes from './routes/projectRoutes'
 
 dotenv.config()
@@ -19,6 +20,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 // Routes
+app.use('/api/auth', authRoutes)
 app.use('/api/projects', projectRoutes)
 
 export default app
