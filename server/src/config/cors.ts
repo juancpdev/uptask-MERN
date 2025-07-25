72e3979bce33452de5c67570e44c81a4ade5e63d
Juan Cruz <juan_pineda_12@hotmail.es>
2025-07-25 19:35:56 +0200

    cors

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,16 +1,18 @@
 import { CorsOptions } from "cors";
 
-export const corsConfig : CorsOptions = {
-    origin : function(origin, callback) {
-        // Lista de orígenes permitidos
-        const whiteList = [process.env.FRONTEND_URL];
+const whiteList = [
+  process.env.FRONTEND_URL,                // URL de Vercel (desde variables de entorno)
+  "http://localhost:5173"                  // Para pruebas locales (si las hacés)
+];
 
-
-        if(whiteList.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Error de CORS'))
-        }
-    },
-    credentials: true // Permite enviar cookies en solicitudes cross-origin
-}
+export const corsConfig: CorsOptions = {
+  origin: function (origin, callback) {
+    console.log("Solicitud desde ORIGIN:", origin);
+    if (!origin || whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
+  credentials: true
+};
